f7661f4bd3cf5a3cc6ed1f4db991fe493f95f7b0
rag2504 <ragraichura12@gmail.com>
2025-02-23 15:53:18 +0530

    first

--- android/app/src/main/kotlin/com/example/darshan/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/darshan/MainActivity.kt
@@ -1,5 +1,6 @@
-package com.example.darshan
+package com.example.flutter_try
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+}
